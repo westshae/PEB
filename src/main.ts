91782a0e9e5f4aa8808fb55c379b02a4c5dbe1d6
westshae <shaewest02@gmail.com>
2021-11-24 20:07:45 +1300

    Added database connection, updated .env.example

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 5000);
 }
