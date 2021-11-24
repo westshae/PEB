1f0e9a70f7fd85c0041c0079c02be523b66397bd
westshae <shaewest02@gmail.com>
2021-11-24 20:11:26 +1300

    Updated script names/removed unneeded

--- src/database-connection.service.ts
+++ src/database-connection.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@nestjs/common';
-import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Injectable } from "@nestjs/common";
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 @Injectable()
 export class DatabaseConnectionService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
-      name: 'default',
-      type: 'postgres',
+      name: "default",
+      type: "postgres",
       host: process.env.DATABASE_HOST,
       port: Number(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USER,
@@ -15,7 +15,7 @@ export class DatabaseConnectionService implements TypeOrmOptionsFactory {
       synchronize: true,
       dropSchema: false,
       logging: true,
-      entities: ['dist/**/*.entity.js'],
+      entities: ["dist/**/*.entity.js"],
     };
   }
-} 
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import 'dotenv/config';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import "dotenv/config";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
