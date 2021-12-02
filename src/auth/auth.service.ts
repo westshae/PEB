69fb78b63122801aeba53a31cef806bc940566f2
westshae <shaewest02@gmail.com>
2021-12-02 15:36:44 +1300

    Fixed code regeex

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -30,17 +30,49 @@ export class AuthService {
 
 
   async checkCode(email:string, code: string){
-    let codeRegex = /^[0-9]{8}$/
+    let codeRegex = /^\b\d{8}\b$/
     let emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if(!email.match(emailRegex) || !code.match(codeRegex)) return false;
-    console.log(email);
+    console.log("FUCK1")
+
+    console.log(email.match(emailRegex));
+    console.log(code.match(codeRegex));
+
+    console.log("REEEEEEEEEEEEE")
+
+    if(email.match(emailRegex) === null) return false;
+    console.log("FUC2")
+
+    if(code.match(codeRegex) === null) return false;
+    console.log("FUCK")
     //TODO
     //Check database values to see if has been used, or fits time period;
     //Change database values
     try{
-      let given = await this.authRepo.findOne({email: email});
-      if(given === undefined) return false;
-      return await bcrypt.compare(code, given.protPass);
+      //Checks if email exists in database
+      let account = await this.authRepo.findOne({email: email});
+      if(account === undefined) return false;
+
+      if(account.passUsed) return false;//Checks that current code hasn't been used
+
+      //Checks that code was used within time required since creation.
+      let date = new Date(account.utcPass);
+      let currentDate = Date.now();
+      let timeDifference = 300000;// 5 minutes in milliseconds
+      if(date.getMilliseconds() + timeDifference < currentDate) return false;
+      
+      let success = await bcrypt.compare(code, account.protPass);//Returns if password was successful or not.
+
+      if(success){
+        console.log(account)
+        account.passUsed = true;
+        this.authRepo.update({email:email}, account);
+        console.log(await this.authRepo.findOne({email:email}));
+        console.log("REEE")
+      }
+
+      //Change values of database after successful comparison
+
+      return success;
     }catch(e){
       console.error(e);
       return false;
