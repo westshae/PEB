dd6e4d86e8b61feda7636091d6387262c3afa943
westshae <shaewest02@gmail.com>
2022-03-07 21:12:29 +1300

    added utility to check/sanatise certain fields, under sanatise.ts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { checkEmail, checkPostID, checkStringContent } from "src/utility/sanitise";
 import { PostsService } from "./posts.service";
 @Controller("posts")
 export class PostsController {
@@ -8,7 +9,7 @@ export class PostsController {
   async getPost(@Query() query) {
     try {
       let postID = query.postID;
-      if (typeof postID !== "string") return;
+      if(!checkPostID(postID)) return false;
       return await this.postsService.getPost(postID);
     } catch (e) {
       console.error(e);
@@ -18,17 +19,9 @@ export class PostsController {
   @Post("create")
   createPost(@Query() query) {
     try {
-      let emailRegex =
-        /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
-
       let content = query.content;
       let email = query.email;
-      console.log("ree");
-      console.log(content);
-      if (typeof content !== "string") return false;
-      console.log("woo");
-      if (email.match(emailRegex) === null) return false;
-      console.log("how");
+      if(!checkEmail(email) || !checkStringContent(content)) return false;
       this.postsService.createPost(email, content);
     } catch (e) {
       console.error(e);
@@ -38,14 +31,11 @@ export class PostsController {
   @Post("delete")
   deletePost(@Query() query){
     try{
-      let emailRegex =
-      /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
 
       let email = query.email;
       let postID = query.id;
-      
-      if (typeof postID !== "string") return false;
-      if (email.match(emailRegex) === null) return false;
+
+      if(!checkEmail(email) || !checkPostID(postID)) return false;
 
       this.postsService.deletePost(email, postID);
     }catch(e){
@@ -59,8 +49,7 @@ export class PostsController {
       let postID = query.id;
       let content = query.content;
 
-      if (typeof postID !== "string") return false;
-      if (typeof content !== "string") return false;
+      if(!checkStringContent(content) || !checkPostID(postID)) return false;
 
       this.postsService.addComment(postID, content);
     } catch (e) {
@@ -71,27 +60,27 @@ export class PostsController {
   @Post("addLike")
   addLike(@Query() query) {
     let postID = query.id;
-    if (typeof postID !== "string") return false;
+    if(!checkPostID(postID)) return false;
     this.postsService.like(postID,true);
   }
   @Post("removeLike")
   removeLike(@Query() query) {
     let postID = query.id;
-    if (typeof postID !== "string") return false;
+    if(!checkPostID(postID)) return false;
     this.postsService.like(postID,false);
 
   }
   @Post("addDislike")
   addDislike(@Query() query) {
     let postID = query.id;
-    if (typeof postID !== "string") return false;
+    if(!checkPostID(postID)) return false;
     this.postsService.dislike(postID,true);
 
   }
   @Post("removeDislike")
   removeDislike(@Query() query) {
     let postID = query.id;
-    if (typeof postID !== "string") return false;
+    if(!checkPostID(postID)) return false;
     this.postsService.dislike(postID,false);
   }
 }
